b544c77bcda46f485c3dd01e46ed544294e27504
Antoine <antoine.villemazet@gmail.com>
2018-11-08 10:17:13 +0100

    Update TER_Antoine/interfacedonnees.h

---
+++ TER_Antoine/interfacedonnees.h
@@ -0,0 +1,16 @@
+#ifndef INTERFACEDONNEES_H
+#define INTERFACEDONNEES_H
+
+#include "carton.h"
+
+static int LIGNES_EN_COURS;
+static Carton CARTON_EN_COURS(int);
+static bool DEBUT;
+static bool FIN;
+static bool PAUSE;
+static bool REPRISE;
+static bool URGENCE;
+static int MEMO_ETAT_PRESENT;
+
+
+#endif // INTERFACEDONNEES_H
